2f83f6e1698bd6e0edcebd01a9ee6f9c701655ec
Leonardo Alonzo <leonardo.a@cliker.mx>
2023-10-25 09:43:38 -0600

    Creando el Formulario

---
+++ src/constants/index.js
@@ -0,0 +1,17 @@
+export const MARCAS = [
+    { id: 1, nombre: "Europeo" },
+    { id: 2, nombre: "Americano" },
+    { id: 3, nombre: "Asíatico" },
+];
+
+
+/* Años */
+const YEARMAX = new Date().getFullYear();
+
+export const YEARS = Array.from(new Array(20), (valor, index) => YEARMAX - index)
+
+/* Planes */
+export const PLANES = [
+    { id:1, nombre:"Europeo" },
+    { id:2, nombre:"Americano" }
+]
