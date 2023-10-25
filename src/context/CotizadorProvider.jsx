b0e9ed7cbf5f32468905417e08df5bbb587bcc24
Leonardo Alonzo <leonardo.a@cliker.mx>
2023-10-25 17:37:21 -0600

    Como pasar datos o funciones en el provider

--- src/context/CotizadorProvider.jsx
+++ src/context/CotizadorProvider.jsx
@@ -5,9 +5,18 @@ const CotizadorContext = createContext();
 
 const CotizadorProvider = ({children}) => {
 
+    const hola = "Hola Mundo"
+
+    const fnHolaMundo = () => {
+        console.log("Hola mundo desde la funcion");
+    }
+
     return (
         <CotizadorContext.Provider
-            value={{}}
+            value={{
+                hola,
+                fnHolaMundo
+            }}
         >
             {children}
         </CotizadorContext.Provider>
