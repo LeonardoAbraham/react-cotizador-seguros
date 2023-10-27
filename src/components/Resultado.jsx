abb3d2bac207c60c5c935eb675c34a97fc7a9206
Leonardo Abraham Alonzo Martinez <leonardo.a@cliker.mx>
2023-10-27 13:24:39 -0600

    Finalizando el Componente de Resultado

--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -6,6 +6,7 @@ const Resultado = () => {
     const { marca, plan, year } = datos;
 
     const [nombreMarca] = MARCAS.filter(m => m.id === Number(marca))
+    const [nombrePlan] = PLANES.filter(p => p.id === Number(plan))
 
     if(resultado === 0) return null
 
@@ -16,10 +17,23 @@ const Resultado = () => {
             </h2>
 
             <p className="my-2">
-                <span className="font-bold">
-                    Marca:
-                </span>
+                <span className="font-bold">Marca: </span>
                 {nombreMarca.nombre}
+            </p>
+
+            <p className="my-2">
+                <span className="font-bold">Plan: </span>
+                {nombrePlan.nombre}
+            </p>
+
+            <p className="my-2">
+                <span className="font-bold">Año del Auto: </span>
+                {year}
+            </p>
+
+            <p className="my-2 text-2xl">
+                <span className="font-bold">Total Cotización: </span>
+                {resultado}
             </p>
 
         </div>
